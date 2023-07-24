479348e9bf1eb1d724d4b6c08813df6c6ff35c6a
gizyyy <gizemyilmaz1988@gmail.com>
2023-07-24 17:05:23 +0200

    rate limiter implementation

--- src/main/kotlin/com/kotlinplayground/application/ApplicationSchoolController.kt
+++ src/main/kotlin/com/kotlinplayground/application/ApplicationSchoolController.kt
@@ -2,11 +2,23 @@ package com.kotlinplayground.application
 
 import com.kotlinplayground.domain.School
 import com.kotlinplayground.domain.Teacher
+import io.github.bucket4j.Bandwidth
+import io.github.bucket4j.Bucket
+import io.github.bucket4j.Refill
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import java.time.Duration
 
 @RestController
 class ApplicationSchoolController(private val applicationService: ApplicationService) {
+
+    private lateinit var bucket: Bucket
+
+    init {
+        val limit = Bandwidth.simple(20, Duration.ofMinutes(1L))
+        bucket = Bucket.builder().addLimit(limit).build()
+    }
 
     @GetMapping("/schools/{id}")
     fun getSchool(@PathVariable id: String): ResponseEntity<School> {
@@ -14,10 +26,15 @@ class ApplicationSchoolController(private val applicationService: ApplicationSer
     }
 
     @PostMapping("/schools")
-    fun addOrChangeSchool(@RequestBody school: School
+    fun addOrChangeSchool(
+        @RequestBody school: School
     ): ResponseEntity<Any> {
-        applicationService.addSchool(school)
-        return ResponseEntity.accepted().build()
+        if (bucket.tryConsume(1)) {
+            applicationService.addSchool(school)
+            return ResponseEntity.accepted().build()
+        }
+
+        return ResponseEntity.status(HttpStatus.TOO_MANY_REQUESTS).build();
     }
 
     @DeleteMapping("/schools/{id}")
